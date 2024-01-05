5fc7e1efb7d92796ebf3fcd54a037211ea746b8a
Tapple Gao <tapplek@gmail.com>
2024-01-05 07:31:17 -0800

    PRNT chunk

--- pyrxbm/binary.py
+++ pyrxbm/binary.py
@@ -1585,6 +1585,120 @@ class PROP:
         }"""
 
 
+@dataclass
+class PRNT:
+    FORMAT: int = 0
+    File: BinaryRobloxFile = None
+    """
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobloxFiles.BinaryFormat.Chunks
+{
+    public class PRNT : IBinaryFileChunk
+    {
+        private const byte FORMAT = 0;
+        private BinaryRobloxFile File;
+
+        public void Load(BinaryRobloxFileReader reader)
+        {
+            BinaryRobloxFile file = reader.File;
+            File = file;
+
+            byte format = reader.ReadByte();
+            int idCount = reader.ReadInt32();
+
+            if (format != FORMAT)
+                throw new Exception($"Unexpected PRNT format: {format} (expected {FORMAT}!)");
+
+            var childIds = reader.ReadInstanceIds(idCount);
+            var parentIds = reader.ReadInstanceIds(idCount);
+            
+            for (int i = 0; i < idCount; i++)
+            {
+                int childId = childIds[i];
+                int parentId = parentIds[i];
+
+                Instance child = file.Instances[childId];
+                Instance parent = (parentId >= 0 ? file.Instances[parentId] : file);
+
+                if (child == null)
+                {
+                    RobloxFile.LogError($"PRNT: could not parent {childId} to {parentId} because child {childId} was null.");
+                    continue;
+                }
+
+                if (parentId >= 0 && parent == null)
+                {
+                    RobloxFile.LogError($"PRNT: could not parent {childId} to {parentId} because parent {parentId} was null.");
+                    continue;
+                }
+
+                child.Parent = (parentId >= 0 ? file.Instances[parentId] : file);
+            }
+        }
+
+        public void Save(BinaryRobloxFileWriter writer)
+        {
+            var file = writer.File;
+            File = file;
+
+            var postInstances = writer.PostInstances;
+            var idCount = postInstances.Count;
+
+            var childIds = new List<int>();
+            var parentIds = new List<int>();
+
+            foreach (Instance inst in writer.PostInstances)
+            {
+                Instance parent = inst.Parent;
+
+                int childId = int.Parse(inst.Referent);
+                int parentId = -1;
+
+                if (parent != null)
+                    parentId = int.Parse(parent.Referent);
+
+                childIds.Add(childId);
+                parentIds.Add(parentId);
+            }
+
+            writer.Write(FORMAT);
+            writer.Write(idCount);
+
+            writer.WriteInstanceIds(childIds);
+            writer.WriteInstanceIds(parentIds);
+        }
+
+        public void WriteInfo(StringBuilder builder)
+        {
+            var childIds = new List<int>();
+            var parentIds = new List<int>();
+
+            foreach (Instance inst in File.GetDescendants())
+            {
+                Instance parent = inst.Parent;
+
+                int childId = int.Parse(inst.Referent);
+                int parentId = -1;
+
+                if (parent != null)
+                    parentId = int.Parse(parent.Referent);
+
+                childIds.Add(childId);
+                parentIds.Add(parentId);
+            }
+
+            builder.AppendLine($"- Format:    {FORMAT}");
+            builder.AppendLine($"- ChildIds:  {string.Join(", ", childIds)}");
+            builder.AppendLine($"- ParentIds: {string.Join(", ", parentIds)}");
+        }
+    }
+}
+"""
+
+
 class BinaryRobloxFileChunk:
     """
     BinaryRobloxFileChunk represents a generic LZ4 - compressed chunk
@@ -1817,7 +1931,7 @@ class BinaryRobloxFile(Instance):  # (RobloxFile):
             elif chunk.ChunkType == b"PROP":
                 handler = PROP()
             elif chunk.ChunkType == b"PRNT":
-                handler = None  # PRNT();
+                handler = PRNT()
             elif chunk.ChunkType == b"META":
                 handler = META()
             elif chunk.ChunkType == b"SSTR":
